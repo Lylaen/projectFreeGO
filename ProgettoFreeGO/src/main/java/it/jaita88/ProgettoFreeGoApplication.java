e67a11e248398bcd397fe4d6ab587ec9bc4ce222
Lylaen <giuliana.mancino_98@libero.it>
2023-07-07 09:55:01 +0200

    New Project

---
+++ ProgettoFreeGO/src/main/java/it/jaita88/ProgettoFreeGoApplication.java
@@ -0,0 +1,13 @@
+package it.jaita88;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoFreeGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoFreeGoApplication.class, args);
+	}
+
+}
---
+++ ProgettoFreeGO/src/test/java/it/jaita88/ProgettoFreeGoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.jaita88;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoFreeGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
