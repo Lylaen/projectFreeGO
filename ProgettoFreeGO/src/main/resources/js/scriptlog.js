96abcbd940a3e72974960e9890b428fa39514464
enricolopoio <enrico.lopoio@gmail.com>
2023-07-19 11:21:55 +0200

    Push dei from e delle immaggini, sezioni: primi,secondi,dessert,antipasti

---
+++ ProgettoFreeGO/src/main/resources/js/scriptlog.js
@@ -0,0 +1,151 @@
+function login() {
+  var username = document.getElementById("loginUsername").value;
+  var password = document.getElementById("password").value;
+
+  if (username === "" || password === "") {
+    alert("Please fill in all fields");
+    return false;
+  } else if (password !== "correctpassword") {
+    // Replace "correctpassword" with the actual correct password
+    var email = prompt(
+      "Incorrect password. Please enter your email to reset your password."
+    );
+    if (email !== null && email !== "") {
+      // Call a function to send a password reset email to the user's email address
+      sendResetPasswordEmail(email);
+    }
+    return false;
+  } else {
+    // Perform login check here
+  }
+}
+
+function sendResetPasswordEmail(email) {
+  // Implement the logic for sending an email here
+  alert(
+    "An email has been sent to " +
+      email +
+      " with instructions to reset your password."
+  );
+}
+
+particlesJS.load("particles-js", "./particles.json");
+
+function togglePasswordVisibility() {
+  const passwordInput = document.getElementById("password");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+}
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000"
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 1,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 1,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 8,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: true,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
+});
